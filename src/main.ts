f302c360a7b7655f380ac8038dae6e097c9dffd0
miruoon <rillakerika@gmail.com>
2022-02-26 12:59:26 +0900

    prettierでフォーマットかけた

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,29 @@
-import "./style.css";
+import './style.css'
 
-const addTaskForm = document.querySelector<HTMLFormElement>("#addTaskForm")!;
-const addTaskInput = document.querySelector<HTMLInputElement>("#addTaskInput")!;
-const taskList = document.querySelector<HTMLUListElement>("#taskList")!;
+const addTaskForm = document.querySelector<HTMLFormElement>('#addTaskForm')!
+const addTaskInput = document.querySelector<HTMLInputElement>('#addTaskInput')!
+const taskList = document.querySelector<HTMLUListElement>('#taskList')!
 
-addTaskForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  addTask(addTaskInput.value);
-  addTaskInput.value = "";
-});
+addTaskForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  addTask(addTaskInput.value)
+  addTaskInput.value = ''
+})
 
 const addTask = (newTask: string) => {
   if (!newTask) {
-    return;
+    return
   }
-  const newTaskItem = document.createElement("li");
-  newTaskItem.innerText = newTask;
-  newTaskItem.addEventListener("click", () => {
-    newTaskItem.classList.toggle("completed");
-  });
+  const newTaskItem = document.createElement('li')
+  newTaskItem.innerText = newTask
+  newTaskItem.addEventListener('click', () => {
+    newTaskItem.classList.toggle('completed')
+  })
 
-  newTaskItem.addEventListener("contextmenu", (e) => {
-    e.preventDefault();
-    newTaskItem.remove();
-  });
+  newTaskItem.addEventListener('contextmenu', (e) => {
+    e.preventDefault()
+    newTaskItem.remove()
+  })
 
-  taskList.appendChild(newTaskItem);
-};
+  taskList.appendChild(newTaskItem)
+}
